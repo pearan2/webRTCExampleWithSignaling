b97c9729664d514ac7055950d91fc82ed3bdf0b6
honlee <kij753@naver.com>
2021-07-28 11:56:21 +0900

    [feat] audio volume control with distance between players

--- front/src/RtcVideo.tsx
+++ front/src/RtcVideo.tsx
@@ -1,7 +1,102 @@
 import { useEffect, useRef, useState } from "react";
 import io, { Socket } from "socket.io-client";
+import Audio from "./Audio";
 
-import Video from "./Video";
+// for game
+interface Vector {
+  x: number;
+  y: number;
+}
+
+interface IPlayer {
+  color: string;
+  centerPos: Vector;
+  radius: number;
+  volume: number;
+  draw: (arg0: CanvasRenderingContext2D) => void;
+}
+
+class Me implements IPlayer {
+  color: string;
+  centerPos: Vector;
+  velocity: Vector;
+  radius: number;
+  originRadius: number;
+  volume: number;
+  analyser: AnalyserNode;
+  isUp: boolean;
+  isDown: boolean;
+  isLeft: boolean;
+  isRight: boolean;
+  constructor(
+    centerPos: Vector,
+    velocity: Vector,
+    stream: MediaStream,
+    radius: number = 10,
+    color = "hsl(" + 360 * Math.random() + ", 50%, 50%)"
+  ) {
+    this.color = color;
+    this.centerPos = centerPos;
+    this.velocity = velocity;
+    this.radius = radius;
+    this.originRadius = radius;
+    this.volume = 0;
+    this.isUp = false;
+    this.isDown = false;
+    this.isLeft = false;
+    this.isRight = false;
+
+    const audioContext = new AudioContext();
+    const source = audioContext.createMediaStreamSource(stream);
+    this.analyser = audioContext.createAnalyser();
+    this.analyser.smoothingTimeConstant = 0.4;
+    this.analyser.fftSize = 1024;
+    source.connect(this.analyser);
+  }
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(
+      this.centerPos.x,
+      this.centerPos.y,
+      this.radius,
+      0,
+      Math.PI * 2,
+      true
+    );
+    ctx.fill();
+    if (this.volume > 20) {
+      ctx.strokeStyle = "#33FF33";
+      ctx.lineWidth = 1;
+      ctx.beginPath();
+
+      ctx.arc(
+        this.centerPos.x,
+        this.centerPos.y,
+        this.radius - ctx.lineWidth / 2,
+        0,
+        Math.PI * 2,
+        true
+      );
+      ctx.stroke();
+    }
+  }
+  update(millis: number) {
+    const array = new Uint8Array(this.analyser.frequencyBinCount);
+    this.analyser.getByteFrequencyData(array);
+    this.volume =
+      array.reduce((acc, cur) => {
+        return acc + cur;
+      }, 0) / array.length;
+    if (this.isUp) this.centerPos.y -= this.velocity.y * millis;
+    if (this.isDown) this.centerPos.y += this.velocity.y * millis;
+    if (this.isLeft) this.centerPos.x -= this.velocity.x * millis;
+    if (this.isRight) this.centerPos.x += this.velocity.x * millis;
+    this.radius = this.originRadius + this.volume / 10;
+  }
+}
+
+//
 
 interface OfferAnswerDto {
   fromClientId: string;
@@ -15,10 +110,18 @@ interface IceDto {
   ice: RTCIceCandidate;
 }
 
-class Peer extends RTCPeerConnection {
+export class Peer extends RTCPeerConnection implements IPlayer {
   connectedClientSocketId: string;
   socketId: string;
   remoteStreams: MediaStream[];
+  dc: RTCDataChannel;
+  connectedAudioElement: HTMLAudioElement | null;
+  //IPlayer
+  color: string;
+  centerPos: Vector;
+  radius: number;
+  volume: number;
+  //
   constructor(
     connectedClientSocketId: string,
     socketId: string,
@@ -28,6 +131,64 @@ class Peer extends RTCPeerConnection {
     this.connectedClientSocketId = connectedClientSocketId;
     this.socketId = socketId;
     this.remoteStreams = [];
+
+    //IPlayer
+    this.color = "#000000";
+    this.centerPos = { x: 0, y: 0 };
+    this.radius = 10;
+    this.volume = 0;
+    //
+    this.dc = this.createDataChannel("dc");
+    this.connectedAudioElement = null;
+    this.ondatachannel = (event) => {
+      const receviedDC = event.channel;
+      receviedDC.onmessage = (event) => {
+        const data = JSON.parse(event.data) as IPlayer;
+        this.color = data.color;
+        this.centerPos = data.centerPos;
+        this.radius = data.radius;
+        this.volume = data.volume;
+      };
+      receviedDC.onopen = (event) => {
+        console.log("dataChannel created");
+      };
+      receviedDC.onclose = () => {
+        console.log("dataChannel closed");
+      };
+    };
+  }
+
+  setConnectedAudioElement(audio: HTMLAudioElement) {
+    this.connectedAudioElement = audio;
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(
+      this.centerPos.x,
+      this.centerPos.y,
+      this.radius,
+      0,
+      Math.PI * 2,
+      true
+    );
+    ctx.fill();
+    if (this.volume > 20) {
+      ctx.strokeStyle = "#33FF33";
+      ctx.lineWidth = 1;
+      ctx.beginPath();
+
+      ctx.arc(
+        this.centerPos.x,
+        this.centerPos.y,
+        this.radius - ctx.lineWidth / 2,
+        0,
+        Math.PI * 2,
+        true
+      );
+      ctx.stroke();
+    }
   }
 }
 
@@ -44,13 +205,20 @@ class PeerManager {
   setPeers: React.Dispatch<React.SetStateAction<Peer[]>>;
   localStream: MediaStream;
   pcConfig: RTCConfiguration | undefined;
+  me: Me;
+  drawContext: CanvasRenderingContext2D;
+  lastUpdateTimeStamp: number;
   constructor(
     socket: Socket,
     localStream: MediaStream,
     setPeers: React.Dispatch<React.SetStateAction<Peer[]>>,
+    drawContext: CanvasRenderingContext2D,
     pcConfig?: RTCConfiguration,
     roomId?: string
   ) {
+    this.me = new Me({ x: 0, y: 0 }, { x: 0.2, y: 0.2 }, localStream);
+    this.lastUpdateTimeStamp = Date.now();
+    this.drawContext = drawContext;
     this.localStream = localStream;
     this.socket = socket;
     this.setPeers = setPeers;
@@ -88,6 +256,7 @@ class PeerManager {
     });
 
     socket.on("needToOffer", (toSocketIds: string[]) => {
+      console.log("needToOfferCalled");
       toSocketIds.forEach((connectedSocketId) => {
         if (connectedSocketId !== this.socket.id) {
           const newPeer = this.createPeerWithEventSetting(
@@ -135,12 +304,41 @@ class PeerManager {
     });
 
     socket.emit("joinRoom", roomId || "honleeExample");
+    //game start
+    //
+    const animationCallBack = () => {
+      this.drawContext.fillStyle = "#000000";
+      this.drawContext.fillRect(0, 0, 800, 600);
+      //this.drawContext.clearRect(0, 0, 800, 600);
+      const dateNow = Date.now();
+      this.me.update(dateNow - this.lastUpdateTimeStamp);
+      this.me.draw(this.drawContext);
+      this.peers.forEach((peer) => {
+        if (peer.dc.readyState === "open")
+          peer.dc.send(JSON.stringify(this.me));
+        peer.draw(this.drawContext);
+
+        if (peer.connectedAudioElement) {
+          const distance = Math.sqrt(
+            Math.pow(this.me.centerPos.x - peer.centerPos.x, 2) +
+              Math.pow(this.me.centerPos.y - peer.centerPos.y, 2)
+          );
+
+          peer.connectedAudioElement!.volume = Math.max(1 - distance / 650, 0);
+        }
+      });
+      requestAnimationFrame(animationCallBack);
+
+      this.lastUpdateTimeStamp = dateNow;
+    };
+    requestAnimationFrame(animationCallBack);
   }
   createPeerWithEventSetting(
     connectedClientSocketId: string,
     socketId: string
   ): Peer {
     const newPeer = new Peer(connectedClientSocketId, socketId, this.pcConfig);
+
     this.localStream.getTracks().forEach((track) => {
       newPeer.addTrack(track, this.localStream);
     });
@@ -194,34 +392,89 @@ const App = () => {
   const [peers, setPeers] = useState<Peer[]>([]);
   const localStream = useRef<MediaStream | null>(null);
   const peerManager = useRef<PeerManager | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     if (localStream.current) return;
+    if (!canvasRef.current) return;
     navigator.mediaDevices
-      .getUserMedia({ video: true })
+      .getUserMedia({ video: false, audio: true })
       .then((stream: MediaStream) => {
         localStream.current = stream;
-        const socket = io("http://localhost:8080");
+        const socket = io("https://under5.site:8080");
+        //const socket = io("http://localhost:8080");
         peerManager.current = new PeerManager(
           socket,
           stream,
           setPeers,
+          canvasRef.current!.getContext("2d")!,
           PeerManager.Config
         );
+
+        const keyEventCallBack = (event: KeyboardEvent) => {
+          const me = peerManager.current!.me;
+
+          switch (event.key) {
+            case "ArrowUp": {
+              if (event.type === "keydown") me.isUp = true;
+              else if (event.type === "keyup") me.isUp = false;
+              break;
+            }
+            case "ArrowLeft": {
+              if (event.type === "keydown") me.isLeft = true;
+              else if (event.type === "keyup") me.isLeft = false;
+              break;
+            }
+            case "ArrowDown": {
+              if (event.type === "keydown") me.isDown = true;
+              else if (event.type === "keyup") me.isDown = false;
+              break;
+            }
+            case "ArrowRight": {
+              if (event.type === "keydown") me.isRight = true;
+              else if (event.type === "keyup") me.isRight = false;
+              break;
+            }
+            default: {
+              break;
+            }
+          }
+        };
+        document.addEventListener("keydown", keyEventCallBack);
+        document.addEventListener("keyup", keyEventCallBack);
       })
       .catch((error) => {
         console.error(`getUserMedia() Error : ${error.toString()}`);
       });
   }, []);
 
+  const setConnectedAudioElementByID = (
+    connectedClientId: String,
+    audio: HTMLAudioElement
+  ) => {
+    const targetPeer = peers.find((peer) => {
+      return connectedClientId === peer.connectedClientSocketId;
+    });
+    if (!targetPeer) return;
+    targetPeer.setConnectedAudioElement(audio);
+  };
+
   return (
     <>
+      <canvas
+        ref={canvasRef}
+        width={800}
+        height={600}
+        style={{ border: "1px solid black" }}
+      ></canvas>
       {peers.map((peer) => {
         return (
-          <Video
+          <Audio
             key={peer.connectedClientSocketId}
             stream={peer.remoteStreams[0]}
-          ></Video>
+            id={peer.connectedClientSocketId}
+            setAudio={setConnectedAudioElementByID}
+          ></Audio>
         );
       })}
     </>
